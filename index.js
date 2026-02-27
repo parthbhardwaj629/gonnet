ee0517faceaa11b15316f63bca0305a1f3c33f4b
parthbhardwaj629 <hello.gonnet@gmail.com>
2026-02-27 13:03:22 +0530

    Privacy

--- index.js
+++ index.js
@@ -112,6 +112,10 @@ app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "public", "index.html"))
 );
 
+app.get("/privacy", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "privacy.html"));
+});
+
 // 🔁 CENTRAL DECISION ROUTE
 app.get("/profile/:uniqueId", async (req, res) => {
   try {

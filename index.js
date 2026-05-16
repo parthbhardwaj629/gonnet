40342c985de999ba24de1fdd27b0a021ddf81531
parthbhardwaj629 <hello.gonnet@gmail.com>
2026-05-16 17:09:01 +0530

    debug razorpay live issue

--- index.js
+++ index.js
@@ -12,6 +12,11 @@ const PDFDocument = require("pdfkit");
 
 require("dotenv").config();
 
+
+
+console.log("KEY =", process.env.RAZORPAY_KEY_ID);
+console.log("SECRET =", process.env.RAZORPAY_KEY_SECRET);
+
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
   key_secret: process.env.RAZORPAY_KEY_SECRET
